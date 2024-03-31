eab1b1aec8d70eafb2c4ded8ef0a2e8337211477
Amanda Mor <amandamorsc@gmail.com>
2024-03-31 15:55:42 +0100

    new trasnformation data for ML

--- data_from_hugging.py
+++ data_from_hugging.py
@@ -12,6 +12,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.svm import SVR
 from sklearn.neural_network import MLPRegressor
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import mean_squared_error, accuracy_score
 from sklearn.linear_model import LogisticRegression
 
@@ -27,14 +28,23 @@ def preprocessing(sentence):
 
     wordsFiltered = [w for w in tokenized_sentence if w not in stopWords]
 
-    return wordsFiltered
+    document = ' '.join(wordsFiltered)
+
+    tfidf_vectorizer = TfidfVectorizer(tokenizer=lambda x: x.split())
+
+    X_tfidf = tfidf_vectorizer.fit_transform([document])
+
+    feature_names = tfidf_vectorizer.get_feature_names_out()
+    tfidf_values = X_tfidf.toarray()[0]
+
+    return feature_names, tfidf_values
 
 def calculate_difficulty(df):
     df = df.copy()
 
-    word_counts = df['tokenized_chapter'].value_counts()
+    word_counts = df['tokenized_chapter_names'].value_counts()
 
-    df['count'] = df['tokenized_chapter'].map(word_counts)
+    df['count'] = df['tokenized_chapter_names'].map(word_counts)
 
     scaler = StandardScaler()
     df['z_scores'] = scaler.fit_transform(pd.DataFrame(df['count']))
@@ -45,9 +55,9 @@ def calculate_difficulty(df):
     return df
 
 
-df_train = pd.read_parquet('train.parquet')
-df_test = pd.read_parquet('test.parquet')
-df_val = pd.read_parquet('validation.parquet')
+df_train = pd.read_parquet('./output/train.parquet')
+df_test = pd.read_parquet('./output/test.parquet')
+df_val = pd.read_parquet('./output/validation.parquet')
 
 df_concat = pd.concat([df_train, df_test, df_val], axis=0)
 
@@ -56,15 +66,17 @@ df_concat['book_id'] = df_concat['book_id'].str.lower()
 df_concat['book_id'] = df_concat['book_id'].str.replace(",", "")
 df_concat['book_id'] = df_concat['book_id'].str.replace(":", "")
 df_concat['book_id'] = df_concat['book_id'].str.replace("\\", "")
+df_concat['chapter'] = df_concat['chapter'].str.replace(",", "")
+df_concat['chapter'] = df_concat['chapter'].str.replace("\\", "")
 
 df_concat['book_id'] = df_concat['book_id'].apply(lambda x: re.search(r'^(.*?)\.', x).group(1) if re.search(r'^(.*?)\.', x) else x)
 df_concat = df_concat.drop_duplicates(subset=['book_id'], keep='first')
 df_concat = df_concat[['book_id', 'chapter']]
-df_concat['tokenized_chapter'] = df_concat['chapter'].apply(preprocessing)
+df_concat[['tokenized_chapter_names', 'tokenized_chapter_values']] = df_concat['chapter'].apply(lambda x: pd.Series(preprocessing(x)))
 
-df_livros_exploded = df_concat.explode('tokenized_chapter')
+df_exploded = df_concat.explode(['tokenized_chapter_names', 'tokenized_chapter_values'])
 
-df_difficulty = calculate_difficulty(df_livros_exploded)
+df_difficulty = calculate_difficulty(df_exploded)
 
 # print(df_difficulty.isna().sum())
 # print(df_difficulty.describe())
@@ -72,11 +84,10 @@ df_difficulty = calculate_difficulty(df_livros_exploded)
 
 ################### MODEL ###################
 
-X = df_difficulty[['count']]  # ,
-y = df_difficulty['difficulty']     # Variável alvo (dificuldade)
+X = df_difficulty[['tokenized_chapter_values']]
+y = df_difficulty['difficulty']    # Variável alvo (dificuldade)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 
 models = [
     # PolynomialFeatures(),
@@ -86,7 +97,7 @@ models = [
     DecisionTreeRegressor(),
     RandomForestRegressor(),
     GradientBoostingRegressor(),
-    SVR(),
+    # SVR(),
     MLPRegressor()
 ]
 
@@ -96,10 +107,8 @@ for model in models:
     print(model)
     model.fit(X_train, y_train)
 
-    # Prevendo a dificuldade das palavras no conjunto de teste
     y_pred = model.predict(X_test)
 
-    # Avaliando a precisão do modelo
     mse = mean_squared_error(y_test, y_pred)
     model_mse.append({'model': model, 'mse': mse})
 
@@ -116,5 +125,16 @@ RandomForestRegressor(): 6.39943491536164e-30
 GradientBoostingRegressor(): 4.744948432629651e-07
 SVR(): 0.007276583661479124
 MLPRegressor(): 8.959357549481053e-05
+"""
+
+"""
+New_model with values per word
+LinearRegression(): 0.04692219800103661
+Ridge(): 0.046929850922370925
+Lasso(): 0.05437747109587966
+DecisionTreeRegressor(): 0.03932792662428544
+RandomForestRegressor(): 0.03880273437033058
+GradientBoostingRegressor(): 0.04044777722332121
+MLPRegressor(): 0.04129191294145817
 """
 
